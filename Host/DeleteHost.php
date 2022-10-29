13c40652627a558176ba43e74302aef6252fc84d
Louka <90894709+loukawstl@users.noreply.github.com>
2022-10-29 22:17:23 +0200

    Add files via upload

---
+++ Host/DeleteHost.php
@@ -0,0 +1,105 @@
+<?php
+    require '../src/Autoloader.php';
+    use App\Connection\Connection;
+    use App\Repository\HostRepository;
+    use App\Validators\HostValidator;
+    use App\Classes\Host;
+
+    
+    //récupération de l'id en get, si il n'y en a pas l'utilisateur est redirigé
+    if(!empty($_GET['id']))
+    {
+        $id = HostValidator::trimData($_GET['id']);
+    }
+    else{
+        header("Location: Host.php");
+    }
+
+    $myArray = HostRepository::initializeArray();
+    $myHost = new Host($id, "", "", "");
+    $myHost = HostRepository::selectHost($myHost);
+
+    if(!empty($_POST)) {
+        //si POST n'est pas vide on essaye de supprimer customer avec deleteCustomer. Cette fonction peut retourner
+        //une erreur si le customer en question est utilisé dans d'autres tables. Les erreurs sont gérés plus bas.
+        $myArray = HostRepository::deleteHost($id);
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Supprimer un Hébergeur </title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" 
+        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+        <script src="JS/main.js"></script>
+        <link rel="stylesheet" href="../CSS/Style.css">
+    </head>
+    
+    <body>
+
+    <?php include '../Layout/Menu.php'; ?>
+
+        <h1 class="text-logo"> Hébergeur </h1>
+        <div class="container admin">
+            <div class="row">
+                <h1><strong> Supression d'un Hébergeur </strong></h1>
+                <br>
+                <form class="form" role="form" method="post">
+                    <input type="hidden" name="id" value="<?php echo $id ?>">
+                    <br>
+                    <?php
+
+                    if (empty($_POST)){
+                        //si post est vide alors le premier message est affiché
+
+                        echo "<p> Voulez-Vous vraiment supprimer l'hébergeur " . $myHost->getName() . " ? Cette action est définitive. </p>";
+                        echo "<div class='form-actions'>";
+                        echo "<button type='submit' class='btn btn-danger'> Supprimer </button> <a class='btn btn-secondary btnAnnuler' href='Host.php'> Annuler </a>";
+                        echo "</div>";
+
+                    }
+                    else{
+                        //en fonction de si l'utilisateur est utilisé dans d'autre table ou non il sera soit supprimé avec succès (isSuccess = 0)
+                        //ou la page retournera une ou des erreurs à partir de myArray
+
+                        if ($myArray['isSuccess'] == 0){
+
+                            echo "<br>";
+                            echo "Hébergeur supprimé avec succès.";
+                            echo "<a class='btn btn-warning' href='Host.php'> Retour </a>";
+
+                        }
+
+                        else{
+
+                            echo"Erreur(s):";
+                            //liste toutes les variables de myArray sauf celles ayant pour clé isSuccess
+                            foreach($myArray as $key=>$value){
+                                if ($key != 'isSuccess'){
+                                    echo "<br>";
+                                    echo $myArray[$key];
+                                }
+                            }
+                            echo "<br>";
+                            echo "<a class='btn btn-warning' href='Host.php'> Retour </a>";
+
+                        }
+
+                    }
+
+                    ?>
+
+                </form>
+            </div>
+        </div>   
+    </body>
+</html>
---
+++ Host/InsertHost.php
@@ -0,0 +1,89 @@
+<?php
+    require '../src/Autoloader.php';
+    use App\Connection\Connection;
+    use App\Repository\HostRepository;
+    use App\Validators\HostValidator;
+    use App\Classes\Host;
+
+    //initialisation de myCustomer
+    $myHost = new Host(0,"","","");
+    //initialisation de l'array utilisé pour retourner les erreurs
+    $myArray = HostValidator::initializeArray();
+
+    if(!empty($_POST)) {
+
+        //myCustomer reçoit les données du formulaire
+        $myHost->setCode(HostValidator::trimData($_POST['code']));
+        $myHost->setName(HostValidator::trimData($_POST['name']));
+        $myHost->setNotes(HostValidator::trimData($_POST['notes']));
+       
+        //la fonction static validateCustomer est utilisé. Cette fonction retourne un array listant les possibles
+        //erreurs retournés des données du formulaire
+        $myArray = HostValidator::validateHost($myHost);
+
+        if ($myArray['isSuccess'] == 0){ //si l'attribut isSuccess de myArray est 0 alors validatesCustomer n'a trouvé aucune erreur
+            HostRepository::insertHost($myHost);
+            header("Location: Host.php");
+        }
+
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Ajouter un Hébergeur </title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" 
+        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+        <script src="JS/main.js"></script>
+        <link rel="stylesheet" href="../CSS/Style.css">
+    </head>
+    
+    <body>
+
+    <?php include '../Layout/Menu.php'; ?>
+
+        <h1 class="text-logo"> Hébergeurs </h1>
+        <div class="container admin">
+            <div class="row">
+                <h1><strong>Ajouter un Hébergeur</strong></h1>
+                <br>
+                <form class="form" role="form" method="post">
+                    <br>
+                    <div>
+                        <label class="form-label" for="code">Code :</label>
+                        <input type="text" class="form-control" id="code" name="code" placeholder="Insérer un code pour l'hébergeur" value="<?php echo $myHost->getCode();?>">
+                        <span class="help-inline"><?php echo $myArray['codeError'];?></span>
+                    </div>
+                    <br>
+                    <div>
+                        <label class="form-label" for="name">Nom :</label>
+                        <input type="text " class="form-control" id="name" name="name" placeholder="Insérer un nom pour l'hébergeur" value="<?php echo $myHost->getName();?>">
+                        <span class="help-inline"><?php echo $myArray['nameError'];?></span>
+                    </div>
+                    <br>
+                    <div>
+                    <label class="form-label" for="notes">Notes :</label>
+                        <textarea id="notes" name="notes" placeholder="Notes" rows="10" cols="173"><?php echo $myHost->getNotes();?></textarea>
+                        <span class="help-inline"><?php echo $myArray['notesError'];?></span>
+                    </div>
+                    <br>
+                    <div class="form-actions">
+
+                        <button type="submit" class="btn btn-success"><span class="bi-pencil"></span> Ajouter</button>
+                        <a class="btn btn-secondary" href="Host.php"> Annuler </a>
+                        
+                    </div>
+                </form>
+            </div>
+        </div>   
+    </body>
+</html>
