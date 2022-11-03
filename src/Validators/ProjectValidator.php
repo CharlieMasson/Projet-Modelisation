6bdee9b1afcb8829cf29cd66e4c078f1a311e765
valentin-CARIA <79085983+valentin-CARIA@users.noreply.github.com>
2022-11-03 23:34:29 +0100

    Add files via upload

---
+++ src/Validators/ProjectValidator.php
@@ -0,0 +1,103 @@
+<?php
+namespace App\Validators;
+
+use App\Classes\Project;
+use App\Validators\Validator;
+
+class ProjectValidator extends Validator{
+
+    public static function initializeArray(): array{
+        $myArray = array(
+            'nameError' => '',
+            'codeError' => '',
+            'lastPassFolderError' => '',
+            'linkMockUpsError' => '',
+            'managedServerError' => '',
+            'notesError' => '',
+            'hostIdError' => '',
+            'customerIdError' => '',
+        );
+        return $myArray;
+    }
+
+    //retourne un array qui contient dans l'ordre: 0/1, erreur code, erreur name, erreur notes
+    public static function validateProject(Project $myProject): array{
+
+        $myArray = array(
+            'isSuccess' => 0,
+            'nameError' => '',
+            'codeError' => '',
+            'lastPassFolderError' => '',
+            'linkMockUpsError' => '',
+            'managedServerError' => '',
+            'notesError' => '',
+            'hostIdError' => '',
+            'customerIdError' => '',
+
+        );
+
+        $isSuccess = true;
+
+        if (Validator::checkEmpty($myProject->getName())) {
+            $myArray['nameError'] = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        else if (Validator::checkLength255($myProject->getName())){
+            $myArray['nameError'] = 'Ce champ ne peut pas contenir plus de 255 caractères.';
+            $isSuccess = false;
+        }
+
+        if (Validator::checkEmpty($myProject->getCode())) {
+            $myArray['codeError'] = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        else if (Validator::checkLength255($myProject->getCode())){
+            $myArray['codeError']= 'Ce champ ne peut pas contenir plus de 255 caractères.';
+            $isSuccess = false;
+        }
+        if (Validator::checkEmpty($myProject->getLastPassFolder())) {
+            $myArray['lastPassFolderError'] = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        else if (Validator::checkLength255($myProject->getLastPassFolder())){
+            $myArray['lastPassFolderError']= 'Ce champ ne peut pas contenir plus de 255 caractères.';
+            $isSuccess = false;
+        }
+        if (Validator::checkEmpty($myProject->getLinkMockUps())) {
+            $myArray['linkMockUpsError'] = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        else if (Validator::checkLength255($myProject->getLinkMockUps())){
+            $myArray['linkMockUpsError']= 'Ce champ ne peut pas contenir plus de 255 caractères.';
+            $isSuccess = false;
+        }
+        if (Validator::checkEmpty($myProject->getManagedServer())) {
+            $myArray['managedServerError'] = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        else if (Validator::checkLength1($myProject->getManagedServer())){
+            $myArray['managedServerError']= 'Ce champ ne peut pas contenir plus de 1 caractères.';
+            $isSuccess = false;
+        }
+
+        if (Validator::checkLength1000($myProject->getNote())){
+            $myArray['notesError'] = 'Ce champ ne peut pas contenir plus de 1000 caractères.';
+            $isSuccess = false;
+        }
+        if (Validator::checkLength1000($myProject->getHostId())){
+            $myArray['hostIdError'] = 'Ce champ ne peut pas contenir plus de 1000 caractères.';
+            $isSuccess = false;
+        }
+        if (Validator::checkLength1000($myProject->getCustomerId())){
+            $myArray['customerIdError'] = 'Ce champ ne peut pas contenir plus de 1000 caractères.';
+            $isSuccess = false;
+        }
+
+        if ($isSuccess == false){
+            $myArray['isSuccess'] = '1';
+        }
+
+        return $myArray;
+
+    }
+}
