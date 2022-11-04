e7c8c24b7ab22b7d6c4933fe5da4eaca32006866
CharlieMasson <95211992+CharlieMasson@users.noreply.github.com>
2022-11-04 14:25:33 +0100

    Update Menu.php

--- Layout/Menu.php
+++ Layout/Menu.php
@@ -8,8 +8,8 @@
 
         <ul class="menu__box">
             <li><a class="menu__item"><i class="fas fa-home"></i> Tableau de bord</a></li>
-            <li><a class="menu__item" href="../Host/Host.php"><i class="fas fa-user-circle"></i> Projets</a></li>
-            <li><a class="menu__item" href="../Host/InsertHost.php"><i class="fas fa-user-circle"></i> Ajouter un Projets</a></li>
+            <li><a class="menu__item" href="../Project/Project.php"><i class="fas fa-user-circle"></i> Projets</a></li>
+            <li><a class="menu__item" href="../Project/InsertProject.php"><i class="fas fa-user-circle"></i> Ajouter un Projets</a></li>
             <li><a class="menu__item" href="../Customer/Customer.php"><i class="fas fa-user-circle"></i> Clients</a></li>
             <li><a class="menu__item" href="../Customer/InsertCustomer.php"><i class="fas fa-user-circle"></i> Ajouter un Client</a></li>
             <li><a class="menu__item" href="../Host/Host.php"><i class="fas fa-check-square"></i> Hebergeurs</a></li>
